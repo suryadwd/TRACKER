d061b02385b83fd41f76007524387f490d9a7a80
Suryakant <surajdwivedi644@gmail.com>
2025-04-03 02:45:55 +0530

    order model

---
+++ models/order.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+
+  customerName: { type: String, required: true},
+  customerAddress: { type: String, required: true},
+  customerPhone: { type: String, required: true},
+  customerLocation: {
+    lat: { type: Number, required: true},
+    lng: { type: Number, required: true} 
+  },
+  driver: { type: mongoose.Schema.Types.ObjectId, ref: 'Driver', required: true},
+
+},{timestamps: true});
+
+module.exports = mongoose.model('Order', orderSchema)
