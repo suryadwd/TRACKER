1bc50d9d933dd4f8efcb3fb7da37cc8add7ead7e
Suryakant <surajdwivedi644@gmail.com>
2025-04-09 09:03:04 +0530

    router for google controller

---
+++ routes/google.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const {loationDetails} = require('../controller/google.controller')
+
+router.get("/location/:orderId", loationDetails);
+
+module.exports = router
